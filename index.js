58a18ad971083554a794fe04a5f96d9547219f65
Skylar <skylarblessings5@gmail.com>
2023-03-13 00:31:08 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function increment(n) {
+    return n++;
+  }
+  
+  function decrement(n) {
+    return n--;
+  }
+  function increment(n) {
+    return n += 1;
+  }
+  
+  function decrement(n) {
+    return n -= 1;
+  }
+ parseInt("2.2222", 10);
+  parseInt("nonsense!", 10);
+  NaN
+  
+  parseFloat("80.123999"); //
+  80.123999
+      
